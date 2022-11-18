34f5145fcb8663ef7e30b781b00e191d956bdb91
Charlie Zhang <cz317@georgetown.edu>
2022-11-17 21:42:50 -0500

    Improve parsing procedures for vu.

--- scripts/PdfParse-vu-visitor-exec.py
+++ scripts/PdfParse-vu-visitor-exec.py
@@ -1,19 +1,18 @@
 from scripts.PdfParse import *
 
 vu_lsts = os.listdir("data/tourism/vanuatu")
-
+dec_lst = [file for file in vu_lsts if "Dec" in file]
 
 error_dict = {
     "file": [],
     "reason": []
 }
 
-error_dict
 
-for file in vu_lsts:
+for file in dec_lst:
     if ".pdf" in file:
-        print(f"{file} has started")
 
+        print(f"{file} has started")
         filepath = os.getcwd() + "/data/tourism/vanuatu/" + file
 
         df = load_pdf(filepath, "Visitor Arrivals by Purpose of Visit", 6)
@@ -22,31 +21,61 @@ for file in vu_lsts:
         df = df.dropna(thresh=4, axis=1).replace("-", 0)
         df = df.iloc[3:].reset_index().drop("index", axis=1)
 
-        splited = df["Conferences Stop Over"].str.split(" ", n=2, expand=True)
-        if len(splited.columns) == 2:
-            splited.columns = ["Conference", "Stopover"]
-        else:
-
-        df = pd.concat([df, splited], axis=1)
-        df = remove_separator(df)
-
         try:
-            df = df.drop(["Conferences Stop Over", "Année", "Mois"], axis=1)
+            col_lst = df.columns.to_list()
+            stored_splited = ["Business, Stop",
+                              "Cruiseship Other", "Conferences Stop Over"]
 
-            if "Holidays" in df.columns:
-                df["Holidays"] = df["Holidays"].str.replace(" ", "")
-                if check_quality(df, ["Year", "Month"], "Visitors"):
-                    saved_path = os.getcwd() + "/data/tourism/vanuatu/temp/" + file.split(".")[0] + ".csv"
+            for idx, val in enumerate(col_lst):
+                if type(val) == str and val in stored_splited:
+                    if val == "Business, Stop":
+                        separate_data(df, "Business, Stop", ",")
+
+                    elif val == "Conferences Stop Over":
+                        splited = df[val].str.split(" ", n=2, expand=True)
+
+                        if len(splited.columns) == 2:
+                            splited.columns = [val.split(" ")[0], val.split(" ")[-1]]
+                            df = pd.concat([df, splited], axis=1)
+
+                        else:
+                            print(f"{file} has incompatible column.")
+                            error_dict["file"].append(file)
+                            error_dict["reason"].append("Incompatible Column")
+
+                    else:
+                        splited = df[val].str.split(" ", n=2, expand=True)
+
+                        if len(splited.columns) == 2:
+                            splited.columns = val.split(" ")
+                            df = pd.concat([df, splited], axis=1)
+                        else:
+                            print("Incompatible Column")
+                            error_dict["file"].append(file)
+                            error_dict["reason"].append("Incompatible Column")
+
+            df = remove_separator(df)
+
+            try:
+                if "Holidays" in df.columns:
+                    df["Holidays"] = df["Holidays"].str.replace(" ", "")
+                    saved_path = os.getcwd() + "/data/tourism/vanuatu/temp/" + \
+                        file.split(".")[0] + ".csv"
                     df.to_csv(saved_path, encoding="utf-8")
 
-            else:
+                else:
+                    print("Holidays column not found.")
+                    error_dict["file"].append(file)
+                    error_dict["reason"].append("Holidays column not found.")
+
+            except:
+                print(f"{file} has an error.")
                 error_dict["file"].append(file)
-                error_dict["reason"].append("Holidays column not found.")
+                error_dict["reason"].append("Année or Mois column not found.")
 
         except:
-            print(f"{file} has an error.")
             error_dict["file"].append(file)
-            error_dict["reason"].append("Année or Mois column not found.")
+            error_dict["reason"].append("Column Error")
 
 
-len(splited.columns)
+error_dict
