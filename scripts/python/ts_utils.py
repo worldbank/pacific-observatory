67d3d7c3a267331379d138019746b29e9994724e
Charlie Zhang <cz317@georgetown.edu>
2023-01-31 18:04:19 -0500

    Upload Google Trends files

--- scripts/python/ts_utils.py
+++ scripts/python/ts_utils.py
@@ -103,20 +103,3 @@ def grangers_causation_matrix(data, variables,
     df.columns = [var + '_x' for var in variables]
     df.index = [var + '_y' for var in variables]
     return df
-
-
-
-def cointegration_test(df, alpha=0.05):
-
-    from statsmodels.tsa.vector_ar.vecm import coint_johansen
-
-    out = coint_johansen(df,-1,5)
-    d = {'0.90':0, '0.95':1, '0.99':2}
-    traces = out.lr1
-    cvts = out.cvt[:, d[str(1-alpha)]]
-    def adjust(val, length= 6):
-        return str(val).ljust(length)
-    # Summary
-    print('Name   ::  Test Stat > C(95%)    =>   Signif  \n', '--'*20)
-    for col, trace, cvt in zip(df.columns, traces, cvts):
-        print(adjust(col), ':: ', adjust(round(trace,2), 9), ">", adjust(cvt, 8), ' =>  ' , trace > cvt)
