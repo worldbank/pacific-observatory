4967f54eb219487b380121f01dad8583e821b38a
Charlie Zhang <cz317@georgetown.edu>
2023-04-10 16:28:12 -0400

    update palau/solomon evaluation metrics

--- scripts/python/tsmodel.py
+++ scripts/python/tsmodel.py
@@ -172,3 +172,29 @@ class SARIMAXPipeline:
             comparison_result["avg_error"].append(model_avg_error)
 
         return comparison_result
+    
+
+def varma_search(data: pd.DataFrame, var_name: list, exog: pd.DataFrame):
+
+    from statsmodels.tsa.api import VARMAX
+    from sklearn.model_selection import ParameterGrid
+
+    param_grid = {'p': [1, 2, 3], 'q': [1, 2, 3], 'tr': ['n', 'c', 't', 'ct']}
+    pg = list(ParameterGrid(param_grid))
+
+    model_res = {
+        "model": [],
+        "result": []
+    }
+    for idx, params in enumerate(pg):
+        print(f' Running for {params}')
+        p = params.get('p')
+        q = params.get('q')
+        tr = params.get('tr')
+        model = VARMAX(data[var_name],
+                       exog=exog,
+                       order=(p, q), trend=tr).fit(disp=False)
+        model_res["model"].append((p, q, tr))
+        model_res["result"].append(model.aic)
+
+    return model_res
