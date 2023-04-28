b2caafd300361721efec2b125d7e64cf46d03909
Charlie Zhang <cz317@georgetown.edu>
2023-04-28 17:35:56 -0400

    Initiate a VARMA pipeline for analysis

--- scripts/python/tsa/mtsmodel.py
+++ scripts/python/tsa/mtsmodel.py
@@ -1,33 +1,94 @@
-from .utsmodel import SARIMAXData
+import pandas as pd
+import numpy as np
+import statsmodels
+import sklearn
+import os
+from scripts.python.tsa.utsmodel import SARIMAXData
+from scripts.python.tsa.ts_eval import *
 
 
-class VARData(SARIMAXData):
+class MultiTSData(SARIMAXData):
+    SELECT_COLS = ["seats_arrivals_total", "seats_arrivals_intl",
+                   "number_of_flights_total", "number_of_flights_intl"]
+
     def __init__(self, country: str, data=None):
         super().__init__(country, data)
-        self.aviation_path = os.getcwd() + "/data/tourism/"
+        self.aviation_path = os.getcwd() + "/data/tourism/aviation_seats_flights_pic.xlsx"
+
+    def read_and_merge(self,
+                       avi_type: str = "passenger",
+                       select_cols: list = SELECT_COLS):
+        
+        # Inherit from the read_and_merge method from SARIMAXData
+        super().read_and_merge()
+        
+        # Process the Aviation Data
+        avi = pd.read_excel(self.aviation_path)
+        avi.columns = [col.lower().replace(" ", "") for col in avi.columns]
+        avi["date"] = pd.to_datetime(avi["date"])
+        avi["country"] = avi["country"].str.lower()
+        avi = (avi[(avi.country == str(self.country)) & (avi.aircraft_type == str(avi_type))]
+               .reset_index()
+               .drop("index", axis=1))
+        
+        avi.index = avi["date"]
+        avi = avi[select_cols].groupby(pd.Grouper(freq='M')).sum()
+        
+        # Drop last month because of the half-month data
+        avi = avi.reset_index().iloc[:-1, :]
+        avi["date"] = avi["date"] - pd.offsets.MonthBegin()
+        
+        self.data = avi.merge(self.data, how="left", on="date")
+        display(self.data.head(5))
 
 
-def varma_search(data: pd.DataFrame, var_name: list, exog: pd.DataFrame):
+class VARPipleline(MultiTSData):
+    def __init__(self, country, var_name, data=None):
+        super().__init__(country, data)
+        self.var_name = var_name
 
-    from statsmodels.tsa.api import VARMAX
-    from sklearn.model_selection import ParameterGrid
+    def test_stationarity(self):
+        from .ts_utils import get_adf_df
+        adf_df = get_adf_df(self.data[self.var_name], self.var_name)
+        
+        order = 0
+        while np.average(adf_df["p-value"]) > 0.05:
+            self.stationary_data = self.data[self.var_name].diff().dropna()
+            adf_df = get_adf_df(self.stationary_data, self.var_name)
+            print(f"order = {order}: no stationarity obtained.")
+            order += 1
+        else:
+           print(f"order = {order}: stationarity obtained.")
+           display(adf_df)
 
-    param_grid = {'p': [1, 2, 3], 'q': [1, 2, 3], 'tr': ['n', 'c', 't', 'ct']}
-    pg = list(ParameterGrid(param_grid))
+    def transform(self):
+        print("Transformation has finished.")
 
-    model_res = {
-        "model": [],
-        "result": []
-    }
-    for idx, params in enumerate(pg):
-        print(f' Running for {params}')
-        p = params.get('p')
-        q = params.get('q')
-        tr = params.get('tr')
-        model = VARMAX(data[var_name],
-                       exog=exog,
-                       order=(p, q), trend=tr).fit(disp=False)
-        model_res["model"].append((p, q, tr))
-        model_res["result"].append(model.aic)
+    def varma_search(self, exog: pd.DataFrame):
 
-    return model_res
+        from statsmodels.tsa.api import VARMAX
+        from sklearn.model_selection import ParameterGrid
+
+        param_grid = {'p': [1, 2, 3],
+                      'q': [1, 2, 3],
+                      'tr': ['n', 'c', 't', 'ct']}
+        pg = list(ParameterGrid(param_grid))
+
+        model_res = {
+            "model": [],
+            "result": []
+        }
+        
+        for idx, params in enumerate(pg):
+            print(f' Running for {params}')
+            p = params.get('p')
+            q = params.get('q')
+            tr = params.get('tr')
+            model = VARMAX(self.data[self.var_name],
+                           exog=self.data[self.exog],
+                           order=(p, q),
+                           trend=tr).fit(disp=False)
+            model_res["model"].append((p, q, tr))
+            model_res["result"].append(model.aic)
+
+        return model_res
