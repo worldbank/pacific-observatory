bd34d8bb95c1cbb7ff1bc6d87f9739da0ee24971
Charlie Zhang <cz317@georgetown.edu>
2023-11-20 10:48:25 -0500

    update text sources (matangi/samoa observer)

--- scripts/scraping/matangi.py
+++ scripts/scraping/matangi.py
@@ -21,7 +21,7 @@ urls_info =  {
     "date": [],
     "title" : [],
     "tag": [],
-    "location": []
+    "location": [],
 }
 for page in urls_raw:
     page_url, page_raw = page[0], page[1]
@@ -29,7 +29,7 @@ for page in urls_raw:
     urls_info["url"].extend([i.find("a")["href"] for i in page_raw[0]])
     urls_info["date"].extend([i.text for i in page_raw[2]])
     urls_info["tag"].extend([i.text for i in page_raw[1]])
-    urls_info["location"].extend([i.text.strip() for i in page_raw[3]]) 
+    urls_info["location"].extend([i.text.strip() for i in page_raw[3]])
 
 mtg_urls = pd.DataFrame(urls_info)
 mtg_urls["url"] = [f"https://matangitonga.to{url}" for url in mtg_urls.url]
@@ -37,5 +37,35 @@ mtg_urls["date"] = pd.to_datetime(mtg_urls["date"])
 mtg_urls.to_csv(f"{target_dir}matangi_urls.csv", encoding="utf-8")
 
 # 
-news_urls = mtg_urls[mtg_urls.location.str.contains("Tonga")]["url"].tolist()
-news_raw = mtg.scrape_urls(news_urls[:10], "field__items", speed_up=True)
+urls_df = pd.read_csv(target_dir+"matangi_urls.csv").drop("Unnamed: 0", axis=1)
+urls_df["tonga"] = urls_df["location"].apply(lambda x: "tonga" in str(x).lower())
+news_urls = urls_df[urls_df["tonga"] == True]["url"].tolist()
+print_urls_raw = mtg.scrape_urls(news_urls, "print-page", speed_up=True)
+
+# Create a mapping between page urls and 
+urls_mapping = []
+for i in print_urls_raw:
+    original_url = i[0]
+    print_url = i[1][0]["href"]
+    urls_mapping.append([original_url, print_url])
+urls_map_df = pd.DataFrame(urls_mapping, columns=["original_url", "print_url"])
+urls_map_df.to_csv(f"{target_dir}matangi_urls_map.csv", encoding="utf-8")
+
+print_urls = urls_map_df["print_url"].tolist()
+news_raw = mtg.scrape_urls(print_urls, "field__items", speed_up=True)
+
+news_info = []
+for raw in news_raw:
+    url = raw[0]
+    if len(raw[1]) > 1:
+        news = raw[1][1].text
+    elif len(raw[1]) == 1:
+        news = raw[1][0].text
+    news_info.append([url, news])
+
+news_df = pd.DataFrame(news_info, columns=["print_url", "news"])
+news_df = (news_df.merge(urls_map_df, how="left", on="print_url")
+                  .merge(urls_df, how="left", 
+                         left_on="original_url", right_on="url"))
+news_df = news_df[["url", "date", "title", "news", "tag"]]
+news_df.to_csv(target_dir+"matangi_news.csv", encoding="utf-8")
---
+++ scripts/scraping/samoa_observer.py
@@ -0,0 +1,54 @@
+import os
+import sys
+sys.path.insert(0, "../../")
+import pandas as pd
+import json
+import requests
+from tqdm import tqdm
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from src.scraper.scrape import WebScraper
+
+target_dir = sys.path[0] + "data/text/samoa/"
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+def load_page(url, timeout=30):
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        output = json.loads(response.content)
+        return output
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to retrieve the page: {e}")
+        pass
+
+urls = [f"https://www.samoaobserver.ws/stories/page/{num}.json?&category=samoa&api=true"
+        for num in range(0, 2079)]
+scraped_data = []
+with tqdm(total=len(urls)) as pbar:
+    max_workers = multiprocessing.cpu_count() + 4
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_url = {executor.submit(load_page, url): (url) for url in urls}
+        for future in as_completed(future_to_url):
+            url = future_to_url[future]
+            try:
+                data = future.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (url, exc))
+            else:
+                scraped_data.append([url, data])
+                pbar.update(1)
+
+url_output = []
+for page in scraped_data:
+    request_url = page[0]
+    json_data = page[1]["stories"]
+    for i in json_data:
+        url_output.append([i["id"], i["heading"], i["published_date"], i["is_premium"]])
+
+so_urls = pd.DataFrame(url_output, columns=["id", "title", "date", "is_premium"])
+so_urls["date"] = pd.to_datetime(so_urls["date"])
+so_urls = so_urls.sort_values(by="date", ascending=True).reset_index(drop=True)
+so_urls["url"] = so_urls["id"].apply(lambda x: f"https://www.samoaobserver.ws/category/samoa/{x}")
+so_urls.to_csv(target_dir+"samoa_observer_urls.csv", encoding="utf-8")
