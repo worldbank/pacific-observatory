f2c2ffb949a2b1fad8645fcd24e40f7c93d6d56c
Jerónimo Luza <jero.luza@gmail.com>
2025-09-23 18:01:30 -0300

    removed sample 50 from thumbnails

--- src/text/scrapers/newspaper_scraper.py
+++ src/text/scrapers/newspaper_scraper.py
@@ -716,7 +716,7 @@ class NewspaperScraper:
             thumbnails = await self.discover_and_scrape_thumbnails()
             
             # Step 3: Scrape full articles
-            articles = await self.scrape_articles(thumbnails[:50])
+            articles = await self.scrape_articles(thumbnails)
             
             # Compile results
             results = {
